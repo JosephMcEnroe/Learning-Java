faee56199f30baf66d92ae0610210dde50a7ea44
JosephMcE890 <jcm1664@rit.edu>
2022-07-21 22:47:39 -0400

    practice 1

---
+++ java1.java
@@ -0,0 +1,9 @@
+public class java1 {
+    public static void main(String[] args)
+    {
+        System.out.println("===blank line===\nHello I'm Joseph McEnroe\n===blank line===\n"
+        +"I'm from Pittsburgh PA\n");
+
+    }
+    
+}
