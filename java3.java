7d2d62c1e0f2b06aa6b6b33dbb7d2bea21e9d655
JosephMcE890 <jcm1664@rit.edu>
2022-07-22 22:52:28 -0400

    LEARNING ARRAYS LETS GO

---
+++ java3.java
@@ -0,0 +1,21 @@
+public class java3 {
+    public static void main(String[] args)
+    {
+        //array
+        int bioArray[] = new int[10];
+         
+        //variables
+        int count;
+        System.out.println(bioArray.length);
+        
+        for (count= 1; count<=bioArray.length; count++);//loop problem c
+        {
+            
+            System.out.println(count);
+            
+        }
+
+
+    }
+    
+}
