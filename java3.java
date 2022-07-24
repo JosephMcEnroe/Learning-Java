7db6f039c97bd7bd90f247096c1fab3c2e351abf
JosephMcE890 <jcm1664@rit.edu>
2022-07-24 17:54:16 -0400

    Okay this is looks good

--- java3.java
+++ java3.java
@@ -8,19 +8,25 @@ public class java3 {
         //array
         String questionsArray[] = {"What is your name?", "What is the name of your first pet?","What was your first pet?"};
         String bioArray[] = new String[3];
+        String sentenceArray[] = {"Your Name is ", "The Name of Your First Pet is ", "Your First Pet was a "};
          
         //variables
-        int count;
-        System.out.println(bioArray.length);
+        int count; //Loop Control
+        
         
         //Questions Loop
-        for(count = 0; count<= questionsArray.length-1; count++)//loop problem c
+        for(count = 0; count<= questionsArray.length-1; count++)//LOOP PROBLEM SOLVED LETS GOO
         {  
             System.out.print(questionsArray[count]+": ");
             String x = reader.readLine();
             bioArray[count]= x;
             System.out.print("\n");
             
+        }
+        //Answer Loop
+        for(count = 0; count <= bioArray.length-1; count++)
+        {
+            System.out.println(sentenceArray[count]+ bioArray[count]+".");
         }
         
         
