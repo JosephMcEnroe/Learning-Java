d1a9e15fdabff9dec7b09db90b7bbac3797e07e8
JosephMcE890 <jcm1664@rit.edu>
2022-08-04 13:13:22 -0400

    OPP

---
+++ hiOPP.java
@@ -0,0 +1,20 @@
+public class hiOPP implements javaOPP{
+    //Superclass
+    
+    public static void main(String[] args){
+        /*javaOPP j1 = new javaOPP();
+        j1.setFlavor("Beef");
+        System.out.println(j1.getFlavor());*/
+
+        hiOPP j1 = new hiOPP();
+        j1.openBag();
+    }
+
+    
+    public void openBag() {
+        System.out.println("Bag Opened");
+        
+    }
+    
+    
+}
---
+++ javaOPP.java
@@ -0,0 +1,22 @@
+public interface javaOPP {
+    //Subclasses
+   //polymorphism just means to have the same name but with different methods
+    //Encapuluation--All below
+   /*private String flavor;
+
+   public void setFlavor(String newFlavor){//Set
+    flavor = newFlavor;
+   }
+   public String getFlavor(){//get
+    return flavor;
+   }
+   public void openBag() {
+    System.out.println("Bag is opened");
+   }*/
+
+   final String flavor = "Beef";
+   void openBag();
+   
+    
+}
+
